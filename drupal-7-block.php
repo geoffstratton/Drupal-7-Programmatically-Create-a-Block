0ae522e75dabd41faee618f8e187688c60caf493
Geoff Stratton <36451347+geoffstratton@users.noreply.github.com>
2019-11-10 11:24:23 -0800

    Initial commit

--- drupal-7-block.php
+++ drupal-7-block.php
@@ -1,9 +1,5 @@
-# Drupal 7: Programmatically Create a Block
- How to programmatically create a block for your Drupal 7 web site
+<?php
 
-So you want to add a block to your module in Drupal 7. First, define its internal name and human-readable title (for the Blocks UI) with hook_block_info():
-
-```php	
 /**
 * Implements hook_block_info().
 */
@@ -15,11 +11,7 @@ function YOUR-MODULE_block_info() {
   );
   return $blocks;
 }
-```
 
-Then, give it the permissions needed to view it, a title (to appear for your site visitors), and some content with hook_block_view():
-
-```php	
 /**
 * Implements hook_block_view().
 */
@@ -38,11 +30,7 @@ function YOUR-MODULE_block_view($delta = '') {
   }
   return $block;
 }
-```
 
-If you want to give the block a visual configurator -- the "Configure" link in the Blocks UI -- beyond what's already provided by the Block module (title, description, visibility settings, etc.), use hook_block_configure() and hook_block_save():
-
-```php	
 /**
  * Implements hook_block_configure()
  */
@@ -68,6 +56,5 @@ function YOUR-MODULE_block_save($delta = '', $edit = array()) {
     variable_set('YOUR-BLOCK-TEXT', $edit['YOUR-BLOCK-TEXT']);
   }
 }
-```
 
-By using variable_set() and variable_get(), when you save some text in your custom field, Drupal will write the text into the variables table in its database and retrieve the text whenever the block is displayed.
+?>
